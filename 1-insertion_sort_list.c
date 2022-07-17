36d70ec2f3ee52160657cd4b48e29712ad6ee26c
Valentine <vchidubem9@gmail.com>
2022-07-17 01:34:30 +0100

    insertion algo

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Insertion Algorithm 
+ * @list: doubly linked list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *start, *rev;
+
+	if (!(*list))
+		return;
+
+    for(start = (*list); start; start = start->next)
+    {
+        while(start->next && start->next->n < start->n)
+        {
+            rev = start->next;
+            start->next = rev->next;
+            rev->prev = start->prev;
+
+            if (rev->next != NULL)
+                rev->next->prev = start;
+
+            if (start->prev != NULL)
+                start->prev->next = rev;
+
+            rev->next = start;
+			start->prev = rev;
+			if (rev->prev == NULL)
+				*list = rev;
+			else
+				start = rev->prev;
+			print_list(*list);
+        }
+    }
+}
