207f9b80aabe385f2fe8136c22c38e0a430fc641
Valentine <vchidubem9@gmail.com>
2022-07-17 13:38:18 +0100

    shell sort algo

---
+++ 100-shell_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Implementation of shell sort algorithm
+ * @array: array of integers
+ * @size: size of array
+ */
+
+void shell_sort(int *array, size_t size)
+{
+    int gap = 1, i, val, idx_min;
+
+    while (gap < (int)size / 3)
+        gap = gap * 3 + 1;
+
+    while (gap > 0)
+    {
+        for (i = gap; i < (int)size; i++)
+        {
+            val = array[i];
+            idx_min = i;
+            while(idx_min >= gap && val < array[idx_min - gap])
+            {
+                array[idx_min] = array[idx_min - gap];
+                idx_min -= gap;
+            }
+            array[idx_min] = val;
+        }
+        gap = (gap - 1) / 3;
+        print_array(array, size);
+    }
+}
