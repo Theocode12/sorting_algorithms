6b163885aa219ca514562eeac5c23f3b6cec2f72
Valentine <vchidubem9@gmail.com>
2022-07-17 16:22:09 +0100

    cocktail sort

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,75 @@
+#include "sort.h"
+
+/**
+ * cocktail_sort_list - Implementation of cocktail sort algorithm
+ * @list: doubly linked list 
+ */
+
+void cocktail_sort_list(listint_t **list)
+{
+    int swap;
+    listint_t *head, *tmp;
+
+    swap = 1;
+    while (swap)
+    {
+        head = *list;
+        swap = 0;
+        while (list && head->next)
+        {
+            if (head->n > head->next->n)
+            {
+                tmp = head->next;
+                head->next = tmp->next;
+                tmp->prev = head->prev;
+
+                if (tmp->next != NULL)
+                    tmp->next->prev = head;
+
+                if (head->prev != NULL)
+                    head->prev->next = tmp;
+
+                head->prev = tmp;
+                tmp->next = head;
+
+                if (tmp->prev == NULL)
+                    *list = tmp;
+                swap = 1;
+                print_list(*list);
+            }
+            else
+                head = head->next;
+        }
+
+        swap = 0;
+        while (head->prev)
+        {
+            if (head->n < head->prev->n)
+            {
+                tmp = head->prev;
+                head->prev = tmp->prev;
+                tmp->next = head->next;
+
+                if (head->next != NULL)
+                {
+                    head->next->prev = tmp;
+                } 
+
+                if (tmp->prev != NULL)
+                {
+                    tmp->prev->next = head;
+                }
+
+                head->next = tmp;
+                tmp->prev = head;
+
+                if (head->prev == NULL)
+                    *list = head;
+                swap = 1;
+                print_list(*list);
+            }
+            else
+                head = head->prev;
+        }
+    }
+}
